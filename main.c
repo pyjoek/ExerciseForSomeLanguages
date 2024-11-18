01d25cfa6a09e1d5ed4cebdff8e0bc6561330dd0
pyyoek <joelnrlson@gmai.com>
2024-11-19 00:10:03 +0300

    initiated the file for C training

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n = 90;
+    char letter = 'K';
+    char name[] = "joel";
+
+    printf("The integer is %d \n", n);
+    printf("The letter is %c \n", letter);
+    printf("The word is %s", name);
+}
