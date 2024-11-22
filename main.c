a86bf4880be959ab2a67f8c6409a9545978ae44e
pyyoek <joelnrlson@gmai.com>
2024-11-23 00:18:03 +0300

    the C basic part

--- main.c
+++ main.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main() {
-    int n = 90;
-    char letter = 'K';
-    char name[] = "joel";
+// #### Basic for C
+// int main() {
+//     int n = 90;
+//     char letter = 'K';
+//     char name[] = "joel";
+//     bool status = 8 == 8;
 
-    printf("The integer is %d \n", n);
-    printf("The letter is %c \n", letter);
-    printf("The word is %s", name);
-}
+//     printf("The integer is %d \n", n);
+//     printf("The letter is %c \n", letter);
+//     printf("The word is %s", name);
+//     printf("%b", status);
+// }
+
+
+// #### Comparing strings
+// int main() {
+//     char name[] = "joel";
+//     char user[10];
+//     scanf("%9s", &user);
+//     if (strcmp(user, name) == 0) {
+//         printf("welcome name correct");
+//     }else{
+//         printf("name incorrect");
+//     }
+// }
