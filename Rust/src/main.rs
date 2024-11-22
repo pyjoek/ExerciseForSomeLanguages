58ebf920cf347b4e11f49064e5b5d6e817d61f2f
pyyoek <joelnrlson@gmai.com>
2024-11-23 00:41:02 +0300

    added the rust folder

---
+++ Rust/src/main.rs
@@ -0,0 +1,22 @@
+/// ##### Basic Variables
+// fn main() {
+//     let name = "Joel";
+//     let age = 12;
+//     let flot = 12.2;
+//     let is_married = false;
+
+//     println!("My name is {}, i'm {} years old and am i married {}", name, age, is_married);
+// }
+
+
+/// ##### if statement
+// fn main() {
+//     let name = "joel";
+//     if name == "joel" {
+//         println!("i wish you were Joel");
+//     }else if name == "Joyce" {
+//         println!("i wish you were Joyce");
+//     } else {
+//         println!("i wish you were nunda");
+//     }
+// }
---
+++ Rust/target/debug/main.d
@@ -0,0 +1,1 @@
+/home/dragon/Documents/Multi/Rust/target/debug/main: /home/dragon/Documents/Multi/Rust/src/main.rs
