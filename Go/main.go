18343fe1c1a31473abd9e45910e13ebfdbb6e388
pyyoek <joelnrlson@gmai.com>
2024-11-24 08:41:31 +0300

    go running the basics here

---
+++ Go/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	name:= "joel";
+	age:= 12;
+	status:= "hacker";
+
+	if (name != "juma") {
+		fmt.Printf("Hello my name is %s, i'm %d years old pursuing the career of %s", name, age, status)
+	}
+}
