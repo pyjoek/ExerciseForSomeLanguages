0b3d8eadce2d8a833ce917a37410e1be294c3bf0
pyyoek <joelnrlson@gmai.com>
2024-11-25 14:38:26 +0300

    got to get user input thrrough console

--- Js/main.js
+++ Js/main.js
@@ -1,15 +1,22 @@
-let name = "joel";
-let age = 12;
-let status = "single";
+// let name = "joel";
+// let age = 12;
+// let status = "single";
 
-if (name === "john") {
-    console.log("Hello, John!");
-}else if (name === "Joshua") {
-    console.log("Hello, Joshua!");
-}else {
-    console.log("Hello, " + name);
-}
+// if (name === "john") {
+//     console.log("Hello, John!");
+// }else if (name === "Joshua") {
+//     console.log("Hello, Joshua!");
+// }else {
+//     console.log("Hello, " + name);
+// }
 
-for (num = 0;num <= 10; num++) {
-    console.log(num);
-}
+// for (num = 0;num <= 10; num++) {
+//     console.log(num);
+// }
+
+
+// ### get user input === install prompt first, :: npm install prompt-sync
+const prompt = require('prompt-sync')();
+let name = prompt("Enter your name: ");
+console.log("Your name is ", name);
+
