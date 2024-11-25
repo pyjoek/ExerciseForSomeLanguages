5319a361fe994c3bcf1df17ea7bb83026a978cc7
pyyoek <joelnrlson@gmai.com>
2024-11-25 14:13:10 +0300

    added the js file

---
+++ Js/main.js
@@ -0,0 +1,11 @@
+let name = "joel";
+let age = 12;
+let status = "single";
+
+if (name === "john") {
+    console.log("Hello, John!");
+}else if (name === "Joshua") {
+    console.log("Hello, Joshua!");
+}else {
+    console.log("Hello, " + name);
+}
