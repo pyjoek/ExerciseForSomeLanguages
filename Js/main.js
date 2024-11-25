9e6ccd56e1357fd413629c16b17e785e68c82b74
pyyoek <joelnrlson@gmai.com>
2024-11-25 14:44:46 +0300

    still damn

--- Js/main.js
+++ Js/main.js
@@ -19,15 +19,3 @@
 const prompt = require('prompt-sync')();
 let name = prompt("Enter your name: ");
 console.log("Your name is ", name);
-
-if (name === "john") {
-    console.log("Hello, John!");
-}else if (name === "Joshua") {
-    console.log("Hello, Joshua!");
-}else {
-    console.log("Hello, " + name);
-}
-
-for (num = 0;num <= 10; num++) {
-    console.log(num);
-}
