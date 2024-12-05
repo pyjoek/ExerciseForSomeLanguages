2f277677d043eeaab07067e0924200b5d3472dfe
pyyoek <joelnrlson@gmai.com>
2024-12-05 23:44:22 +0300

    started the loop in python

--- Python/main.py
+++ Python/main.py
@@ -2,10 +2,20 @@ name = "joel"
 age = 12
 married = False
 
-if name != "joel":
-    print("You're not Joel")
-else:
-    if age > 18:
-        print("You're an adult")
-    else:
-        print("Passed everything")
+# === IF STATEMENT
+# if name != "joel":
+#     print("You're not Joel")
+# else:
+#     if age > 18:
+#         print("You're an adult")
+#     else:
+#         print("Passed everything")
+
+# === WHILE LOOP
+# while True:
+#     print("Hello, World!") # this will print forever
+
+count = 0
+while count != 5:
+    print("Hello, World!") # this will print then add 1, and when it get's to 5 is stops
+    count += 1
