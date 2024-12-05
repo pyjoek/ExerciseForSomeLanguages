9d6985a6b00603895486e0c1265acf524ae661ae
pyyoek <joelnrlson@gmai.com>
2024-12-05 23:49:01 +0300

    the for loop just happened

--- Python/main.py
+++ Python/main.py
@@ -15,7 +15,16 @@ married = False
 # while True:
 #     print("Hello, World!") # this will print forever
 
-count = 0
-while count != 5:
-    print("Hello, World!") # this will print then add 1, and when it get's to 5 is stops
-    count += 1
+# == WHILE LOOP WITH A CONDITION
+# count = 0
+# while count != 5:
+#     print("Hello, World!") # this will print then add 1, and when it get's to 5 is stops
+#     count += 1
+
+# == FOR LOOP
+for i in range(10):
+    print("Hello world") # this will print according to the number in the brancket of range function
+
+names = ['joel', 'john', 'joshua'] # here we prepare the names
+for name in names: # here we take each name one by one
+    print(f"Hello {name}") # print hello then each name, each in a new line
