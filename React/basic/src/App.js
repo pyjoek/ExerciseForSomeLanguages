663dc938254b79be527e10071e7ec11af18da314
pyyoek <joelnrlson@gmai.com>
2024-12-07 23:47:24 +0300

    In react introduced the magic of react in form element

--- React/basic/src/App.js
+++ React/basic/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import {useState} from 'react';
+// import Counts from './comps/count'
+import Fom from './comps/formName'
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-     <h1>You clicked the button {count} Times</h1>
-     <button onClick={() => setCount(count + 1)}>Increase</button>
+     {/* <Counts /> */} 
+     <Fom />
     </div>
   );
 }
